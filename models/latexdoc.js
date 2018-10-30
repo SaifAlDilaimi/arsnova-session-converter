d0ccb3dbd6197b0ad13d6dcbfae04ebce2290091
SaifAlDilaimi <saif.al-dilaimi@uv.rub.de>
2018-10-30 11:55:09 +0100

    v1

--- models/latexdoc.js
+++ models/latexdoc.js
@@ -62,7 +62,7 @@ class LaTeXDoc{
 
         fse.outputFileSync(texDocOutputPath, this.doc);
         
-        var cmd = 'pdflatex -interaction=nonstopmode -output-directory=./tmp/ '+texDocOutputPath;
+        var cmd = '/net/vmits0310/disc1/texlive/2018/bin/x86_64-linux/pdflatex -interaction=nonstopmode -output-directory=./tmp/ '+texDocOutputPath;
         try {
             execSync(cmd, {shell: '/usr/bin/bash', stdio:[0,1,2]});
         } catch (err) {
