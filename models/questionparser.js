3b6573547a7832d46b412a12a8ac9309e413d31f
SaifAlDilaimi <saif.al-dilaimi@uv.rub.de>
2018-11-13 09:23:07 +0100

    multiple image options

--- models/questionparser.js
+++ models/questionparser.js
@@ -41,7 +41,7 @@ class QuestionParser {
                 }
                 
                 if(questionRecord.image != null){
-                    imageDownloadOptions.push(this.generateDownloadOption(questionRecord.image));
+                    imageDownloadOptions.push([this.generateDownloadOption(questionRecord.image)]);
                 }
                 
                 console.log(imageDownloadOptions[0])
